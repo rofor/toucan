1826bffce58c1fe28c781f02c0765a4a1585625f
rofor <rofor@protonmai.com>
2020-05-23 18:24:55 +0100

    output all values every n seconds

--- readOBD.py
+++ readOBD.py
@@ -5,91 +5,100 @@ import time
 # define variables
 connection = obd.Async("/dev/ttyUSB1")
 
-# callbacks for each sensor
-def new_speed(r):
-    speed = r.value.magnitude
+# global variables declared so not empty
+
+
+# global variables to hold values
+latest_speed = {}
+latest_rpm = {}
+latest_engine_load = {}
+latest_coolant_temp = {}
+latest_intake_pressure = {}
+latest_intake_temp = {}
+latest_maf = {}
+latest_distance_w_mil = {}
+latest_fuel_rail_pressure_direct = {}
+latest_commanded_egr = {}
+latest_fuel_level = {}
+latest_barometric_pressure = {}
+
+# loop
+def myLoop():
     message = {
-            "speed": speed
-                }
-    print(message)
-    
-def new_rpm(r):
-    rpm = r.value.magnitude
-    message = {
-            "rpm": rpm
-                }
-    print(message)
-    
-def new_engine_load(r):
-    engine_load = r.value.magnitude
-    message = {
-            "engine_load": engine_load
-                }
-    print(message)
-    
-def new_coolant_temp(r):
-    coolant_temp = r.value.magnitude
-    message = {
-            "coolant_temp": coolant_temp
-                }
-    print(message)
-     
-def new_intake_pressure(r):
-    intake_pressure = r.value.magnitude
-    message = {
-            "intake_pressure": intake_pressure
-                }
-    print(message)
-    
-def new_intake_temp(r):
-    intake_temp = r.value.magnitude
-    message = {
-            "intake_temp": intake_temp
-                }
-    print(message)
-    
-def new_maf(r):
-    maf = r.value.magnitude
-    message = {
-            "maf": maf
-                }
-    print(message)
-    
-def new_distance_w_mil(r):
-    distance_w_mil = r.value.magnitude
-    message = {
-            "distance_w_mil": distance_w_mil
-                }
-    print(message)
-    
-def new_fuel_rail_pressure_direct(r):
-    fuel_rail_pressure_direct =  r.value.magnitude
-    message = {
-            "fuel_rail_pressure_direct": fuel_rail_pressure_direct
-                }
-    print(message)
-    
-def new_commanded_egr(r):
-    commanded_egr = r.value.magnitude
-    message = {
-            "commanded_egr": commanded_egr
-                }
-    print(message)
-    
-def new_fuel_level(r):
-    fuel_level = r.value.magnitude
-    message = {
-            "fuel_level": fuel_level
-                }
-    print(message)
-    
-def new_barometric_pressure(r):
-    barometric_pressure = r.value.magnitude
-    message = {
-            "barometric_pressure": barometric_pressure
+        "commanded_egr": latest_commanded_egr,
+        "speed": latest_speed,
+        "rpm": latest_rpm,
+        "engine_load": latest_engine_load,
+        "coolant_temp": latest_coolant_temp,
+        "intake_pressure": latest_intake_pressure,
+        "intake_temp": latest_intake_temp,
+        "maf": latest_maf,
+        "distance_w_mil": latest_distance_w_mil,
+        "fuel_rail_pressure_direct": latest_fuel_rail_pressure_direct,
+        "commanded_egr": latest_commanded_egr,
+        "fuel_level": latest_fuel_level,
+        "barometric_pressure": latest_barometric_pressure
                 }
     print(message)
 
+
+# callbacks for each sensor
+def new_speed(r):
+    global latest_speed
+    latest_speed = r.value.magnitude
+    
+def new_rpm(r):
+    global latest_rpm
+    latest_rpm = r.value.magnitude
+
+    
+def new_engine_load(r):
+    global latest_engine_load
+    latest_engine_load = r.value.magnitude
+   
+    
+def new_coolant_temp(r):
+    global latest_coolant_temp
+    latest_coolant_temp = r.value.magnitude
+    
+     
+def new_intake_pressure(r):
+    global latest_intake_pressure
+    latest_intake_pressure = r.value.magnitude
+    
+    
+def new_intake_temp(r):
+    global latest_intake_temp
+    latest_intake_temp = r.value.magnitude
+    
+    
+def new_maf(r):
+    global latest_maf
+    latest_maf = r.value.magnitude
+    
+def new_distance_w_mil(r):
+    global latest_distance_w_mil
+    latest_distance_w_mil = r.value.magnitude
+    
+def new_fuel_rail_pressure_direct(r):
+    global latest_fuel_rail_pressure_direct
+    latest_fuel_rail_pressure_direct =  r.value.magnitude
+    
+    
+def new_commanded_egr(r):
+    global latest_commanded_egr
+    latest_commanded_egr = r.value.magnitude
+    
+    
+def new_fuel_level(r):
+    global latest_fuel_level
+    latest_fuel_level = r.value.magnitude
+    
+    
+def new_barometric_pressure(r):
+    global latest_barometric_pressure
+    latest_barometric_pressure = r.value.magnitude
+   
     
 # callbacks will fire upon receipt of new values
 connection.watch(obd.commands.SPEED, callback=new_speed)
@@ -107,6 +116,10 @@ connection.watch(obd.commands.BAROMETRIC_PRESSURE, callback=new_barometric_press
 
 # start connection
 connection.start()
+
+while True:
+    myLoop()
+    time.sleep(3)
 
 time.sleep(60000)
 connection.stop()
